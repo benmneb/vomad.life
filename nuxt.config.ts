52b5280377f83855eb15f57c31970e81fc466781
benmneb <62878664+benmneb@users.noreply.github.com>
2022-09-18 13:43:47 +0800

    chore: add `nuxt-image`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['@nuxt/content'],
+	modules: ['@nuxt/content', '@nuxt/image-edge'],
 	css: ['@/assets/styles/baseline.scss'],
 	vite: {
 		css: {
@@ -12,6 +12,16 @@ export default defineNuxtConfig({
 						'@use "@/assets/styles/_colours.scss" as *; @use "@/assets/styles/_typography.scss" as *; @use "@/assets/styles/_transitions.scss" as *;',
 				},
 			},
+		},
+	},
+	image: {
+		dir: 'assets/images',
+		screens: {
+			// max-width in px
+			sm: '640',
+			md: '1024',
+			lg: '1920',
+			xl: '2280',
 		},
 	},
 })
