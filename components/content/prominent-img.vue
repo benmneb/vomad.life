28f2f0177fbf661cbcb72dc174f10b5fc143a5d3
benmneb <62878664+benmneb@users.noreply.github.com>
2022-12-28 17:15:15 +0800

    fix(compoents/prominent-img): add figure and figcaption elements and correct styling

--- components/content/prominent-img.vue
+++ components/content/prominent-img.vue
@@ -8,20 +8,39 @@ defineProps({
 		type: String,
 		default: '',
 	},
+	caption: {
+		type: String,
+	},
 })
 </script>
 
 <template>
-	<nuxt-img :src="`content/${src}.webp`" :alt="`${alt} @ https://Vomad.Life`" />
+	<figure>
+		<nuxt-img
+			:src="`content/${src}.webp`"
+			:alt="`${alt} @ https://Vomad.Life`"
+		/>
+		<figcaption v-if="caption">{{ caption }}</figcaption>
+	</figure>
 </template>
 
 <style scoped lang="scss">
-img {
-	object-fit: contain;
+figure {
 	width: 80vw;
-	max-height: 80vh;
-
-	margin: 1rem 0;
+	margin: 2rem 0;
 	margin-left: calc(-1 * (80vw - (800px - 2rem)) / 2);
+
+	img {
+		width: 100%;
+		max-height: calc(80vh - 2rem); // 2rem for the margin
+		object-fit: contain;
+	}
+
+	figcaption {
+		text-align: center;
+		font-style: italic;
+		max-width: 1200px;
+		margin: 1rem auto 0;
+	}
 }
 </style>
