b47b7eeebd08496350b74e24cb9a4c0a847fb82e
benmneb <62878664+benmneb@users.noreply.github.com>
2023-03-01 20:55:58 +0800

    chore: add `nuxt-icon`package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['@nuxt/content', '@nuxt/image-edge'],
+	modules: ['@nuxt/content', '@nuxt/image-edge', 'nuxt-icon'],
 	css: ['@/assets/styles/baseline.scss'],
 	vite: {
 		css: {
