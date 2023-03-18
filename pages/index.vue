be1a27776da895a49ddbaf6c8ab5704a1b6b1447
benmneb <62878664+benmneb@users.noreply.github.com>
2023-03-18 18:49:04 +0800

    feat(pages/index): add title and meta description

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+	title: 'Vomad Life',
+	meta: [
+		{
+			name: 'description',
+			content:
+				'Welcome to the Vomad Life - Aussie Plant-Based Slow-Travelling Digital Nomads Explore the World and bring you Guides, Articles, Surveys, Best Of Lists, Book Reviews and more',
+		},
+	],
+})
+</script>
+
 <template>
 	<main>
 		<hero imgSrc="home/hero.webp">Travel world.<br />Eat plants.</hero>
