633d395354b5f09f8ca32f04954c62a8d95f2c2a
benmneb <62878664+benmneb@users.noreply.github.com>
2023-04-02 12:14:31 +0800

    chore: comment out nitro preset netlify to see if it builds on netlify as good as it does locally

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	nitro: {
-		preset: 'netlify',
-	},
+	// nitro: {
+	// 	preset: 'netlify',
+	// },
 	modules: ['@nuxt/content', '@nuxt/image-edge', 'nuxt-icon'],
 	css: ['@/assets/styles/baseline.scss'],
 	vite: {
